ea42dc3bec7889282c9254e724d7ab84dee9d1a5
Joanna Buchała <joanna.b2@op.pl>
2019-02-05 00:55:46 +0100

    script done and styles

--- js/script.js
+++ js/script.js
@@ -1,47 +1,17 @@
 
 var prefix = "https://cors-anywhere.herokuapp.com/";
-var url = prefix + "http://recruitment-api.pyt1.stg.jmr.pl";
+var url = prefix + "https://recruitment-api.pyt1.stg.jmr.pl";
 
 
-/* {
+  /*login : "correct_login@example.com",
+  password : "C0rr3Ct_P@55w0rd"*/
 
-  login : "correct_login@example.com",
-  password : "C0rr3Ct_P@55w0rd"
-
-}*/
-
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			if (xhr.status == 200) {
-				console.log(xhr.responseText);
-			}
-		}
-
-		if (xhr.status == 404) {
-			console.log('file not found');
-		}
-	};
-
-	var params = {
-
-		  login : "correct_login@example.com",
-		  password : "C0rr3Ct_P@55w0rd"
-
-		}
-
-	var data = JSON.stringify(params)
-	xhr.open('POST', url + '/login');
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(data);
-
-
-/*document.getElementById('send').addEventListener('click', function(e) {
+document.getElementById('send').addEventListener('click', function(e) {
 	e.preventDefault();
 
 	var params = {
-		login : "correct_login@example.com",
- 		password : "C0rr3Ct_P@55w0rd"
+		login : document.getElementById('login').value,
+ 		password : document.getElementById('password').value
 	}
 
 	var xhr = new XMLHttpRequest();
@@ -49,25 +19,42 @@ var url = prefix + "http://recruitment-api.pyt1.stg.jmr.pl";
 	xhr.open('POST', url + '/login');
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.onload = function() {
-			console.log(this.responseText);
+			if (this.readyState == 4 && this.status == 200) {
+				console.log(xhr.responseText);
+				closeForm();
+			} else {
+				alert('wrong password or login')
+			}
 		}
 	xhr.send(JSON.stringify(params));
-});*/
+
+});
 
 
-
+//Open form to sign up
 var form = document.getElementById('form');
 
 function openForm() {
 	form.style.display = 'block';
 }
 
-var close = document.getElementById('close');
-
-close.onclick = function() {
-	form.style.display = 'none';
-}
-
 document.getElementById('enter').addEventListener('click', function() {
 	openForm();
 });
+
+//Close form, set message in button
+
+function closeForm() {
+
+	form.style.display = 'none';
+	var login = document.getElementById('login').value = '';
+	var password = document.getElementById('password').value = '';
+	var message = document.getElementById('enter');
+
+}
+
+var close = document.getElementById('close');
+
+close.onclick = function () {
+	form.style.display = 'none';
+}
